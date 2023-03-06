d6756a634c8a7d39ab8b523306b98000dc2f06dd
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-06 07:20:56 +0200

    Solved Problem No.1 (BeeCrowed Site)

---
+++ BeeCrowed/( 1000 ) Hello World/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        print();
+    } 
+    
+    static void print(){
+       System.out.println("Hello World!");
+    }
+}
+/*
+    System.out.println("Hello World!");
+
+*/
