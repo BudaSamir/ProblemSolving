1410acaa4bf931b572f11ebfbedf3358970437c3
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-06 07:25:11 +0200

    Solved Problem No.2 (BeeCrowed Site)

---
+++ BeeCrowed/( 1001 ) Extremely Basic/Extremely_Basic/src/extremely_basic/Extremely_Basic.java
@@ -0,0 +1,13 @@
+package extremely_basic;
+import java.util.Scanner;
+public class Extremely_Basic {
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int A = in.nextInt();
+       int B = in.nextInt();
+       int X = A + B;
+       System.out.println("X = " + X);
+    }
+    
+}
