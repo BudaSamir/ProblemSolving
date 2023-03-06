0cd43f05be36ef2ac16b7863b944900be29fd089
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-06 08:51:01 +0200

    Solved Problem No.17 (BeeCrowed)

---
+++ BeeCrowed/( 1016 ) Distance/Distance/src/distance/Distance.java
@@ -0,0 +1,11 @@
+package distance;
+import java.util.Scanner;
+public class Distance {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int distance = in.nextInt();
+        System.out.println( ( distance *2 )+" minutos");
+    }
+    
+}
