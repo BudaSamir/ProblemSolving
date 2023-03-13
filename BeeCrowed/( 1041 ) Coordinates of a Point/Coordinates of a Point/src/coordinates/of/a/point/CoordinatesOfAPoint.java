efaaf794c2a0711133f683fd1a79824e6284fe34
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-13 07:22:46 +0200

    Solved Problem No.28(BeeCrowed)

---
+++ BeeCrowed/( 1041 ) Coordinates of a Point/Coordinates of a Point/src/coordinates/of/a/point/CoordinatesOfAPoint.java
@@ -0,0 +1,37 @@
+package coordinates.of.a.point;
+
+import java.util.Scanner;
+
+public class CoordinatesOfAPoint {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        } else {
+            if (x > 0 && y > 0) {
+                System.out.println("Q1");
+
+            } else if (x < 0 && y < 0) {
+                System.out.println("Q3");
+
+            } else if (x < 0 && y > 0) {
+                System.out.println("Q2");
+
+            } else if (x > 0 && y < 0) {
+                System.out.println("Q4");
+
+            } else if ((x > 0 || x < 0) && y == 0) {
+                System.out.println("Eixo X");
+
+            } else if (x == 0 && (y > 0 || y < 0)) {
+                System.out.println("Eixo Y");
+
+            }
+        }
+    }
+
+}
