6bb9a0a5a76848affd068a060c29ade4604fd0b8
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-20 14:59:05 +0200

    Solved Problem No.32 (BeeCrowed)

--- BeeCrowed/( 1045 ) Triangle Types/Triangle Types/src/triangle/types/TriangleTypes.java
+++ BeeCrowed/( 1045 ) Triangle Types/Triangle Types/src/triangle/types/TriangleTypes.java
@@ -9,20 +9,52 @@ public class TriangleTypes {
         double A = in.nextDouble();
         double B = in.nextDouble();
         double C = in.nextDouble();
+        double X;
+        if (A > C && C > B || A > C && C > B) {
+            X = B;
+            B = C;
+            C = X;
+        } else if (B > A && A > C || B > A && A == C ) {
+            X = A;
+            A = B;
+            B = X;
+        } else if (C > B && B > A || C > B && B == A ) {
+            X = A;
+            A = C;
+            C = X;
+        } else if (B > C && C > A || B > C && C == A ) {
+            X = A;
+            A = B;
+            B = X;
+            X = C;
+            C = B;
+            B = X;
+        } else if (C > A && A > B || C > A && A == B ) {
+            X = A;
+            A = C;
+            C = X;
+            X = B;
+            B = C;
+            C = X;
+        }
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
 
         if (A >= (B + C)) {
             System.out.println("NAO FORMA TRIANGULO");
 
-        } else if ((A * A) == (B * B) + (C * C)) {
+        } else if ((A * A) == ((B * B) + (C * C))) {
             System.out.println("TRIANGULO RETANGULO");
 
-        } else if ((A * A) > (B * B) + (C * C)) {
+        } else if ((A * A) > ((B * B) + (C * C))) {
             System.out.println("TRIANGULO OBTUSANGULO");
 
-        } else if ((A * A) < (B * B) + (C * C)) {
+        } else if ((A * A) < ((B * B) + (C * C))) {
             System.out.println("TRIANGULO ACUTANGULO");
 
         }
+
         if (A == B && B == C && A == C) {
             System.out.println("TRIANGULO EQUILATERO");
 
@@ -30,6 +62,7 @@ public class TriangleTypes {
             System.out.println("TRIANGULO ISOSCELES");
 
         }
+
     }
 
 }
